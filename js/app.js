cbf7ac9b009f4c1394e72ddb4ce4c46a52792ee0
phansiri <phansiri@uw.edu>
2014-10-20 11:57:48 -0700

    added stuff before moment.js

--- js/app.js
+++ js/app.js
@@ -13,11 +13,24 @@
  * */
 function onReady() {
     //get a reference to the form
+    var ageForm = document.getElementById('age-form');
 
     //add an event listener for the 'submit' event passing onSubmit as the event handler function
+    ageForm.addEventListener('submit', onSubmit);
 
     //add an event listener for the 'click' event on the exit button
     //for this one we will use an inline anonymous function so that you can get used to those
+    var exitButton = document.getElementById('exit-button');
+    exitButton.addEventListener('click', function() {
+        if (window.confirm('Are you sure?!?')) {
+            window.location = 'http://www.bing.com';
+        }
+    });
+
+    var resetButton = document.getElementById('reset-button');
+    resetButton.addEventListener('click', function() {
+        document.getElementById('age-message').style.display = 'none';
+    })
 
 } //onReady()
 
@@ -38,10 +51,21 @@ function onSubmit(eventObject) {
     //remember that 'this' refers to the object that raised the event (i.e., the form)
 
     //get the name and the date-of-birth value
+    var name = this.elements['name'].value;
+    var dob = this.elements['dob'].value;
 
-    //calculate the age
+    console.log(dob);
 
-    //display the name and age
+    try {
+        //calculate the age
+        var age = calculateAge(dob);
+        //display the name and age
+        displayAge(name, age);
+    }
+    catch(exception) {
+        displayError(exception);
+    }
+
 
     //if the event object has a method called preventDefault,
     //call it to stop the browser from submitting the form
@@ -69,7 +93,21 @@ function onSubmit(eventObject) {
  *   age in years [number]
  */
 function calculateAge(dob) {
+    if (!dob) {
+        throw new Error('Please tell me when you were born!');
+    }
     //calculate the person's age based on the date-of-birth
+    var today = new Date();
+    dob = new Date(dob);
+    var yearsDiff = today.getFullYear() - dob.getUTCFullYear();
+    var monthsDiff = today.getMonth() - dob.getUTCMonth();
+    var daysDiff = today.getDay() - dob.getUTCDay();
+
+    if (monthsDiff < 0 || (0 === monthsDiff && daysDiff < 0)) {
+        yearsDiff--;
+    }
+
+    return yearsDiff;
 
 } //calculateAge()
 
@@ -83,6 +121,8 @@ function calculateAge(dob) {
 function displayAge(name, age) {
     //use displayMessage() to display the name and age
 
+    displayMessage(name + ', you are ' + age + ' years old!');
+
 } //displayAge()
 
 /* displayAge()
@@ -93,7 +133,7 @@ function displayAge(name, age) {
  * */
 function displayError(error) {
     //use displayMessage to display the error
-
+    displayMessage(error, true);
 } //displayError()
 
 /* displayMessage()
@@ -104,5 +144,8 @@ function displayError(error) {
  *   isError - [boolean, default=false] set to true if this is an error message
  * */
 function displayMessage(message, isError) {
-
+    var msgElem = document.getElementById('age-message');
+    msgElem.innerHTML = message;
+    msgElem.className = isError ? 'alert alert-danger' : 'alert alert-success';
+    msgElem.style.display = 'block';
 } //displayMessage()
